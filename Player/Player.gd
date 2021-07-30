f10fe1f0e15f2f01e17614cd1b483a209313ba7c
Yoga Prasetiyo <yoga.xv@gmail.com>
2021-07-30 23:33:59 +0700

    Create Animation Player

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,14 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+# define animation player cara 1 :
+# var animationPlayer = null
+# func _ready():
+#	animationPlayer = $AnimationPlayer
+
+# define animation player cara 2 :
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +21,14 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	# move_and_collide(velocity * delta)
